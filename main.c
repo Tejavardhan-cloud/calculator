8765246c975bf0b55c75ae99c68427bab26f6c1a
Tejavardhan-cloud <tejarjun71@gmail.com>
2026-03-17 23:08:57 +0530

    added div

--- main.c
+++ main.c
@@ -3,6 +3,12 @@ int add(int a,int b)
 {
     return a+b;
 }
+int div(int a,int b)
+{
+    if(a==0)
+    return -1;
+    else return a/b;
+}
 void main()
 {
     printf("welcome\n");
@@ -11,4 +17,5 @@ void main()
     scanf("%d%d,&a,&b");
     int c=add(a,b);
     printf("%d",c);
+    printf("%d\n",div(a,b));
 }
