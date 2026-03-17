14431599ec74dfa6c0a36b657a0325bc8a1a1e4f
Tejavardhan-cloud <tejarjun71@gmail.com>
2026-03-17 22:35:50 +0530

    added addition function

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
+int add(int a,int b)
+{
+    return a+b;
+}
 void main()
 {
     printf("welcome\n");
+    int a,b;
+    printf("enter the a and b values\n");
+    scanf("%d%d,&a,&b");
+    int c=add(a,b);
+    printf("%d",c);
 }
